3069682a6fc3e7607217c4241ebf0530d53006cc
Mimian <42294148+mimians@users.noreply.github.com>
2023-04-22 19:39:47 +0300

    frontend

---
+++ frontend/src/components/home/FeaturesSection.vue
@@ -0,0 +1,194 @@
+<template>
+    <main>
+        <div class="home-width">
+            <div class="features">
+                <div class="feature">
+                    <div class="box">
+                        <div class="text">
+                            <h3>Control earnings on supplied asset.</h3>
+                            <p>As a lender, Create lend offer and control earnings to be earned on supplied assets by
+                                setting loan terms.</p>
+                            <RouterLink to="/portfolio/lends/create">
+                                <div class="action">
+                                    <div class="div"></div>
+                                    <p>Start Lending</p>
+                                </div>
+                            </RouterLink>
+                        </div>
+                        <div class="image">
+                            <img src="/images/lending.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="feature">
+                    <div class="box">
+                        <div class="image">
+                            <img src="/images/borrowing.png" alt="">
+                        </div>
+                        <div class="text">
+                            <h3>Access Fixed-Term Loans on-chain.</h3>
+                            <p>Borrow loans with predictable terms, to be paid back anytime within the loan duration
+                                assigned.</p>
+                            <RouterLink to="/discover">
+                                <div class="action">
+                                    <div class="div"></div>
+                                    <p>Start Borrowing</p>
+                                </div>
+                            </RouterLink>
+                        </div>
+                    </div>
+                </div>
+                <div class="feature">
+                    <div class="soon">Coming Soon</div>
+                    <div class="box">
+                        <div class="text">
+                            <h3>Earn emissions on supplied assets.</h3>
+                            <p>Idle assets Supplied on Darsh, earns yield from trusted third party liquidity protocol, while
+                                being matched to a borrower.</p>
+                            <a target="_blank" href="https://darshprotocol.gitbook.io/product-docs/product-guide/lending">
+                                <div class="action">
+                                    <div class="div"></div>
+                                    <p>Learn More</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="image">
+                            <img src="/images/emission.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    display: flex;
+    justify-content: center;
+}
+
+.features {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+}
+
+.feature {
+    min-height: 960px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    position: relative;
+    /* transform: translateY(-300px); */
+}
+
+.feature .box {
+    width: 1050px;
+    display: flex;
+    flex-wrap: wrap;
+    background: #FFFFFF;
+}
+
+.box .text,
+.box .image {
+    width: 50%;
+    height: 455px;
+}
+
+.text {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    padding: 50px;
+    text-align: center;
+}
+
+.feature:nth-child(odd) .text {
+    border-right: 1px solid #F1F1F1;
+}
+
+.feature:nth-child(even) .text {
+    border-left: 1px solid #F1F1F1;
+}
+
+.text h3 {
+    font-family: 'Axiforma Bold';
+    font-weight: 600;
+    font-size: 38px;
+    color: #39375C;
+}
+
+.text p {
+    font-weight: 500;
+    font-size: 16px;
+    color: #ABADAE;
+    margin-top: 20px;
+    margin-bottom: 50px;
+}
+
+.text .action {
+    height: 50px;
+    min-width: 200px;
+    border: 1px solid rgba(105, 54, 245, 0.4);
+    border-radius: 4px;
+    position: relative;
+    overflow: hidden;
+}
+
+.action p {
+    position: absolute;
+    top: -8px;
+    font-size: 16px;
+    color: var(--primary);
+    width: 100%;
+    transition: .3s;
+}
+
+.action div {
+    background: var(--primary);
+    width: 100%;
+    height: 50px;
+    position: absolute;
+    bottom: -50px;
+    transition: .3s;
+}
+
+.action:hover p {
+    color: #FFFFFF;
+}
+
+.action:hover div {
+    bottom: 0;
+}
+
+.image {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.image img {
+    width: 385px;
+}
+
+.animate {
+    transform: translateY(0);
+}
+
+.soon {
+    height: 30px;
+    background: #6936F5;
+    border-radius: 2px 2px 0px 0px;
+    position: absolute;
+    padding: 0 20px;
+    top: 225px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 16px;
+    color: var(--textnormal);
+}
+</style>
---
+++ frontend/src/components/home/HomeHeader.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="app_section">
+    <main id="main_header">
+      <div class="header">
+        <div class="logo">
+          <img src="../../assets/images/logo2.png" alt="">
+          <img src="/images/logo4.png" alt="">
+        </div>
+        <div class="pages">
+          <a href="#">About</a>
+          <a href="https://darshprotocol.gitbook.io/product-docs" target="_blank">Docs</a>
+          <a href="#">Governance</a>
+          <a href="#">Litepaper </a>
+        </div>
+        <div class="actions">
+          <RouterLink to="/discover">
+            <div class="connect_wallet">
+              <p>Launch App</p>
+            </div>
+          </RouterLink>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<script>
+export default {
+  mounted() {
+    this.determineGlass()
+    const _this = this
+    window.addEventListener('scroll', function () {
+      _this.determineGlass()
+    })
+  },
+  methods: {
+    determineGlass() {
+      const header = document.getElementById('main_header')
+      header.classList.toggle('glass', window.scrollY > 700)
+    }
+  }
+}
+</script>
+
+<style scoped>
+main {
+  top: 0;
+  width: 100%;
+  height: 90px;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  border-image-source: linear-gradient(to left, transparent, #28233A, transparent);
+  z-index: 10;
+  background: rgba(20, 17, 37, 0.02);
+  backdrop-filter: blur(8.5px);
+  transition: .2s;
+  border-left: 0;
+  border-right: 0;
+  border-top: 0;
+  border-bottom: 1px solid;
+  border-image-slice: 1;
+}
+
+.glass {
+  background: #FFFFFF !important;
+  border-image-source: linear-gradient(to left, transparent, #CCCAD3, transparent);
+}
+
+.header {
+  width: 1180px;
+  max-width: 90%;
+  display: grid;
+  grid-template-columns: 200px auto 200px;
+}
+
+.logo {
+  display: flex;
+  align-items: center;
+}
+
+.logo img {
+  width: 124px;
+  height: 30px;
+}
+
+.logo img:last-child {
+  display: none;
+}
+
+.glass .logo img:last-child {
+  display: block;
+}
+
+.glass .logo img:first-child {
+  display: none;
+}
+
+.pages {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+}
+
+.pages a {
+  font-weight: 500;
+  font-size: 16px;
+  color: #989898;
+  text-decoration: none;
+}
+
+.glass .pages a {
+  color: #39375C;
+}
+
+.actions {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 20px;
+}
+
+.connect_wallet {
+  height: 50px;
+  padding: 0 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: .2s;
+  background: rgba(255, 255, 255, 0.04);
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  border-radius: 4px;
+}
+
+.connect_wallet:hover,
+.icon_badge:hover {
+  transform: translateY(-2px);
+}
+
+.connect_wallet p {
+  font-weight: 500;
+  font-size: 16px;
+  color: var(--textnormal);
+}
+
+
+.glass .connect_wallet {
+  border: 1px solid #F1F1F1;
+}
+
+
+.glass .connect_wallet p {
+  color: #39375C;
+}
+
+
+.icon_badge {
+  width: 40px;
+  height: 40px;
+  border-radius: 4px;
+  background: var(--bglight);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  user-select: none;
+  transition: .2s;
+}
+</style>
